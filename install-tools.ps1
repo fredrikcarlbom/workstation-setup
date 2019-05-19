fe2b6511188983a928bfcdbd2ecbc3b100670e49
Fredrik Carlbom <fredrik.carlbom@gmail.com>
2019-05-19 23:22:20 +0200

    Added git-fork

--- install-tools.ps1
+++ install-tools.ps1
@@ -1,6 +1,7 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 choco install git -y
+choco install git-fork -y
 choco install autohotkey -y
 choco install dropbox -y
 choco install totalcommander -y
