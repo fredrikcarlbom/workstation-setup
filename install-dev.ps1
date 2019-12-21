400a0b2a895b30103782703600016d3013d47eda
Fredrik Carlbom <fredrik.carlbom@gmail.com>
2019-12-21 23:20:00 +0100

    Added dev setup

---
+++ install-dev.ps1
@@ -0,0 +1,14 @@
+#Requires -RunAsAdministrator
+
+choco install git -y
+choco install git-fork -y
+choco install vscode -y
+choco install vscode-csharp -y
+choco install vscode-intellicode -y
+choco install vscode-gitignore -y
+choco install vscode-gitlens -y
+choco install vscode-codespellchecker -y
+choco install vscode-mssql -y
+choco install vscode-csharpextensions -y
+choco install vscode-tslint -y
+choco install vscode-eslint -y
--- install-tools.ps1
+++ install-tools.ps1
@@ -1,10 +1,10 @@
 #Requires -RunAsAdministrator
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-choco install git -y
-choco install git-fork -y
+choco install 7zip -y
 choco install autohotkey -y
 choco install dropbox -y
+choco install notepadplusplus -y
 choco install totalcommander -y
 choco install vlc -y
 choco install skype -y
