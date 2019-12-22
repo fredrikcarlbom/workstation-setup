e1169fbb314d680faf60068e2bb9e3eda09e0446
Fredrik Carlbom <fredrik.carlbom@gmail.com>
2019-12-22 08:41:16 +0100

    Additional tools

--- install-tools.ps1
+++ install-tools.ps1
@@ -4,10 +4,12 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.We
 choco install 7zip -y
 choco install autohotkey -y
 choco install conemu -y
+choco install discord -y
 choco install dropbox -y
 choco install notepadplusplus -y
 choco install totalcommander -y
 choco install vlc -y
 choco install skype -y
 choco install firefox -y
+choco install uBlockOrigin-firefox -u
 choco install spotify -y
